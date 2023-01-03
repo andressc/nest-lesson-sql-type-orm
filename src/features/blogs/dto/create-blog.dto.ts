d087457fb1b3068a5c091d82757e4898b027648a
andrejrebrov <674511674511aaqqww>
2023-01-03 19:49:30 +0300

    Lesson 20 fix 4

--- src/features/blogs/dto/create-blog.dto.ts
+++ src/features/blogs/dto/create-blog.dto.ts
@@ -1,20 +1,23 @@
-import { IsNotEmpty, Matches, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { trim } from '../../../common/helpers';
 
 export class CreateBlogDto {
 	@IsNotEmpty()
-	@Transform(({ value }) => trim(value))
+	@IsString()
+	@Transform(({ value }) => trim(value.toString()))
 	@MaxLength(15)
 	name: string;
 
 	@IsNotEmpty()
+	@IsString()
 	@Matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
 	@MaxLength(100)
 	websiteUrl: string;
 
 	@IsNotEmpty()
-	@Transform(({ value }) => trim(value))
+	@IsString()
+	@Transform(({ value }) => trim(value.toString()))
 	@MaxLength(500)
 	description: string;
 }
